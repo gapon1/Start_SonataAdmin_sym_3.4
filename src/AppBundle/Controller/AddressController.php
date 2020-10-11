59459592f48ca12eb5543d732db216b0f1568326
Vitaliy <gapon007@ukr.net>
2020-10-11 21:42:41 +0300

    Create Filters for address List

--- src/AppBundle/Controller/AddressController.php
+++ src/AppBundle/Controller/AddressController.php
@@ -16,24 +16,10 @@ class AddressController extends Controller
         $em = $this->getDoctrine()->getManager();
         $addresses = $em->getRepository('AppBundle:Address')
         ->findAll();
-//
-//        var_dump($addresses);
-//        die();
-
-        /**
-         * @var $paginator
-         */
-        $paginator = $this->get('knp_paginator');
-
-        $result = $paginator->paginate(
-            $addresses,
-            $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 3)
-        );
 
         return $this->render('address/address.html.twig',
             [
-                'addresses' => $result
+                'addresses' => $addresses
             ]);
     }
 }
