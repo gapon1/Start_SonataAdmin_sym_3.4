0eee7bfde05369cef6cf5b2bed3a99a7800bfa5d
Vitaliy <gapon007@ukr.net>
2020-12-27 13:36:40 +0200

    Add new Sell Table

--- src/AppBundle/Admin/AddressAdmin.php
+++ src/AppBundle/Admin/AddressAdmin.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 
 namespace AppBundle\Admin;
 
+use AppBundle\Controller\AddressController;
+use AppBundle\Entity\Address;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use AppBundle\Form\Type\ImageType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 class AddressAdmin extends AbstractAdmin
 {
@@ -48,6 +53,7 @@ class AddressAdmin extends AbstractAdmin
 
     protected function configureFormFields(FormMapper $formMapper): void
     {
+        $image = new Address();
 
         $formMapper
             ->add('createdAt')
@@ -67,10 +73,32 @@ class AddressAdmin extends AbstractAdmin
             ->add('contactPerson')
             ->add('percent')
             ->add('description')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'allow_file_upload' => '/uploads/uploads/',
+                'data_class' => null
+        ])
             ->add('state')
             ->add('comment')
         ;
+    }
+
+    public function prePersist($image)
+    {
+        $this->manageFileUpload($image);
+    }
+
+    public function preUpdate($image)
+    {
+        $this->manageFileUpload($image);
+    }
+
+    private function manageFileUpload($image)
+    {
+//        var_dump($image);
+//        die();
+        if ($image->getFile()) {
+            $image->refreshUpdated();
+        }
     }
 
     protected function configureShowFields(ShowMapper $showMapper): void
